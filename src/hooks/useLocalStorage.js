9607ab5063c240755aac1436b98d5109fe6a5d37
kika <anamonteiro430@gmail.com>
2020-04-05 19:09:08 +0100

    final

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,10 +6,6 @@ import { useState } from 'react';
 // update localStorage when needed
 // Don't forget to parse data from localStorage, and stringify new data getting stored
 export const useLocalStorage = (key, initialValue) => {
-	if (typeof key !== 'string') {
-		throw new Error('invalid');
-	}
-
 	const [storedValue, setStoredValue] = useState(() => {
 		if (localStorage.getItem(key)) {
 			return JSON.parse(window.localStorage.getItem(key));
